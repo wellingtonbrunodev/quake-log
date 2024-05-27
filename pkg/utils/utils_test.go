4a034787825215f74ace64fe5faa8b11893f298e
Wellington <brunoufop@msn.com>
2024-05-27 20:14:11 -0300

    now pretty printing report

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -3,9 +3,9 @@ package utils
 import "testing"
 
 func TestReadFiles(t *testing.T) {
-	content := ReadFile("./input_files/test.log")
-	var expected = "Hello, World!"
-	if content != expected {
+	content := ReadFile("./../input_files/test.log")
+	var expected = []string{"Hello, World!"}
+	if content[0] != expected[0] {
 		t.Fatalf(`Message should be = %s, but is %s`, expected, content)
 	}
 }
